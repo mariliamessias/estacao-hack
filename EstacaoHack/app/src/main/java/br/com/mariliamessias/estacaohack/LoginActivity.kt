0ecdbd548e9f2ccc46160cc98604fabd2453ded5
Maríllia Aparecida Messias de Oliveira <marilia.oliveira@outlook.com>
2020-02-12 21:54:56 -0300

    criacao da logica do login
    Aula 05 - 12/02 Criação da Lógica do login e da splashScreen

--- EstacaoHack/app/src/main/java/br/com/mariliamessias/estacaohack/LoginActivity.kt
+++ EstacaoHack/app/src/main/java/br/com/mariliamessias/estacaohack/LoginActivity.kt
@@ -2,11 +2,29 @@ package br.com.mariliamessias.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnEntrarLogin.setOnClickListener {
+
+            val usuario = edtUsuarioLogin.text.toString().trim()
+            val senha = edtSenhaLogin.text.toString().trim()
+
+            if(usuario.isEmpty()){
+                Toast.makeText(this@LoginActivity, "Preencha o usuário", Toast.LENGTH_SHORT).show()
+            }else if (senha.isEmpty()){
+                Toast.makeText(this@LoginActivity, "Preencha a senha", Toast.LENGTH_SHORT).show()
+            }else if(usuario !=  "admin" || senha != "123"){
+                Toast.makeText(this@LoginActivity, "Usuário ou senha inválido", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this@LoginActivity, "Usuário e senha corretos! :)", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- EstacaoHack/app/src/main/java/br/com/mariliamessias/estacaohack/SplashActivity.kt
+++ EstacaoHack/app/src/main/java/br/com/mariliamessias/estacaohack/SplashActivity.kt
@@ -1,12 +1,26 @@
 package br.com.mariliamessias.estacaohack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //para a thread principal e executa uma outra tread
+        //parametros do metodo postDelayed do o que vai executar e quanto tempo
+        //Intent a intencao de fazer alguma coisa
+        //          intent explicita -> falamos o que ela vai executar (sua acao)
+        //          intent implicita -> camera do celular (exemplo)
+        Handler().postDelayed({
+
+            //o valor de parametro da intent é onde esta // o segundo parametro é para qual activity irá
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
